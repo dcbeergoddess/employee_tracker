cd100b468720b246773d1a17683bba9e5a6b5370
Rachel Murray <dcbeergoddess@gmail.com>
2020-05-11 20:09:59 -0400

    app.js connected to sql

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const chalk = require("chalk");
 //look up 'console.table' - npm package to format data in console
 
 //sql boilerplate
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "CMS_DB"
+});
 
+connection.connect(err => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n"); //THIS IS OPTIONAL
+  connection.end;  
+});
 //ADD
   //departments
   //roles
--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ USE CMS_DB;
 -- #1 DEPARTMENT TABLE
 CREATE TABLE departments (
   department_id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY(department_id)
 );
 
