830d41862edface33d880b8ae56d494807de63c1
Rachel Murray <dcbeergoddess@gmail.com>
2020-05-12 02:25:03 -0400

    got figlet to work to bring employee manager as block lettering to console

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
-//look up 'console.table' - npm package to format data in console
+const figlet = require('figlet');
+const cTable = require('console.table');
 
 //sql boilerplate
 const connection = mysql.createConnection({
@@ -14,9 +15,55 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n"); 
-  connection.end;  
+  console.log("connected as id " + connection.threadId + "\n");
+  figlet('Employee \n Manager', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
 });
+  start(); 
+});
+
+function start() {
+  connection.end;
+}
+
+//Initial Start Questions
+
+// function start() {
+//   inquirer.prompt([
+//     {
+//       type: "list",
+//       message: "What would you like to do?",
+//       name: "",
+//       choices: [
+//         chalk.magenta("Manager"),
+//         chalk.yellow("Engineer"),
+//         chalk.blueBright("Intern"),
+//         chalk.greenBright("Finished")
+//       ]
+//     }
+//   ]).then(response => {
+//     // process all the answers
+//     if (response.type === chalk.magenta("Manager")) {
+//       createManager("Manager");
+//     } else if (response.type === chalk.yellow("Engineer")) {
+//       createEngineer("Engineer");
+//     } else if (response.type === chalk.blueBright("Intern")) {
+//       createIntern("Intern");
+//     } else {
+//       console.info(chalk.green("All Finished!"))
+//       // console.log(teamMembers)
+//       // console.log(idArray)
+//       connection.end();
+//     }
+//   })
+// }
+
+
 //ADD
   //departments
   //roles
