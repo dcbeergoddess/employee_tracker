0b74c9b577fbbfb10adb0028227a2b1449bd31e2
Rachel Murray <dcbeergoddess@gmail.com>
2020-05-13 00:25:09 -0400

    added some more functionality and choices for employee info. Still need to work with table data and pushing it properly to the database

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# employee_tracker
-MySQL: Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as Content Management Systems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
+# EMPLOYEE TRACKING SYSTEM
+
+**Link to GitHub Repo Here =>** [![GitHub commit activity](https://img.shields.io/github/commit-activity/m/dcbeergoddess/employee_tracker)](https://github.com/dcbeergoddess/employee_tracker)
+
+## Description
+
+Creating an interface/app to make it easy for non-developers to view and interact with employee information stored in databases. You should be able to view employees, roles and add employees to the database among other functionality in tracking your employees, departments and roles in multiple tables within an SQL database. 
+
+## Table Of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribuiting](#contributing)
+* [Tests](#tests)
+* [Question](#questions)
+
+## Installation
+
+Install dependencies using command bellow
+- `npm install`
+
+## Usage
+
+- Run the Schema and Seed commands in Workbench!
+
+- Run | **`node app.js`** |
+in your TERMINAL/BASH
+
+## License
+
+MIT License
+
+## Contributing
+
+n/a
+
+## Tests
+
+You will also need to run the schema.sql and seed.sql files withinyour mySQL workbench to work with existing database
+
+## Questions
+
+![Image of dcbeergoddess](https://avatars0.githubusercontent.com/u/59098488?v=4&s=200)
+* [**Contact Me**](mailto:dcbeergoddess@gmail.com?subject=employee_tracker)
--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
   console.log(chalk.blue('================\n  Employee\n  Manager\n================'));
   start();
 });
@@ -25,10 +24,11 @@ function start() {
   inquirer.prompt([
     {
       type: "list",
-      message: "What would you like to do?",
       name: "startchoice",
+      message: "What would you like to do?",
       choices: [
         chalk.magenta("View All Employees"),
+        chalk.green("View All Roles"),
         chalk.yellow("Add Employee"),
         chalk.blueBright("Update Employee Role"),
         chalk.greenBright("Finished")
@@ -37,6 +37,8 @@ function start() {
   ]).then(answer => {
     if (answer.startchoice === chalk.magenta("View All Employees")) {
       viewEmployees();
+    } else if (answer.startchoice === chalk.green("View All Roles")) {
+      viewRoles();
     } else if (answer.startchoice === chalk.yellow("Add Employee")) {
       addEmployee();
     } else if (answer.startchoice === chalk.blueBright("Update Employee Role")) {
@@ -52,7 +54,7 @@ function start() {
 function viewEmployees() {
 
   //SQL Language for Table Joins
-  const allEmployees = "SELECT employees.id, first_name, last_name, title, department_name, salary, manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id;";
+  const allEmployees = 'SELECT employees.id, first_name, last_name, title, department_name, salary, manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id;';
 
   connection.query(allEmployees, function (err, res) {
     if (err) throw err;
@@ -63,9 +65,59 @@ function viewEmployees() {
 //VIEW ALL EMPLOYEES BY DEPARTMENT
 //VIEW ALL EMPLOYEES BY MANAGER
 
+function viewRoles() {
+  connection.query("SELECT title FROM roles", function (err, res) {
+    if (err) throw err;
+    console.log(res);
+    start();
+  });
+}
 
 //ADD EMPLOYEE
-
+function addEmployee() {
+  inquirer
+    .prompt([{
+      name: "first_name",
+      type: "input",
+      message: "Please enter first name.",
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "Please enter last name.",
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "Please choose a role.",
+      choices: [
+        'Owner',
+        'Director of Operations',
+        'Tap Room Manager',
+        'Lead Brewer',
+        'Sales Director',
+        'Tap Room Bartender',
+        'Brewer',
+        'Sales Rep',
+        'HR',
+      ]
+    },
+    ]).then(function (answers) {
+      connection.query(
+        "INSERT INTO employees SET ?",
+        {
+          first_name: answers.first_name,
+          last_name: answers.last_name,
+          // role_id: answers.role,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log("SUCCESS")
+          start();
+        }
+      )
+    });
+}
 
 //REMOVE EMPLOYEE
 
